f9aaac04bf3f1053abbb9c8c573a4182fab17c31
park sangbeom <smctkdqja@gmail.com>
2020-02-07 23:58:52 +0900

    update

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,1 +1,23 @@
-export default () => "Home";
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const GET_MOVIES = gql`
+query{
+  movies{
+    id
+    medium_cover_image
+	}
+}
+`;
+
+
+export default () => {
+    const { loading, error, data } = useQuery(GET_MOVIES);
+    if (loading){
+        return "loading..";
+    }
+    if(data && data.movies){
+        return data.movies.map(movie => <h1>{movie.id}</h1>);
+    }
+}
